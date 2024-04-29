65bdf300c520d570b773d95665af1c28b9ad338a
Alicen Bender <alicene.bender@gmail.com>
2024-04-29 17:09:37 -0400

    restarted code, added requirments for categoryjs and product js

--- ecommerce-back-end/config/connection.js
+++ ecommerce-back-end/config/connection.js
@@ -1,11 +1,10 @@
-const Sequelize = require('sequelize');
-
 require('dotenv').config();
+
+const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  // ennv file was not working so put in my incredibly encrpyted password for mysql
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, 'Love', {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
