8df7b4a9a399d5cc257b863f8b5dec1af8b902fb
Alicen Bender <alicene.bender@gmail.com>
2024-04-29 17:18:12 -0400

    Put the wrong requirments in category and moved them to productjs

--- ecommerce-back-end/models/Category.js
+++ ecommerce-back-end/models/Category.js
@@ -15,29 +15,6 @@ Category.init(
     category_name: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
-    price: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        isDecimal: true
-      }
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      validate: {
-        isNumeric: true
-      }
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'category',
-        key: 'id',
-        unique: false
-      }
     }
   },
   {
--- ecommerce-back-end/models/Product.js
+++ ecommerce-back-end/models/Product.js
@@ -19,6 +19,30 @@ Product.init(
       type: DataTypes.STRING,
       allowNull: false,
   },
+  price: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      isDecimal: true
+    }
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 10,
+    validate: {
+      isNumeric: true
+    }
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'category',
+      key: 'id',
+      unique: false
+    }
+  }
+},
   {
     sequelize,
     timestamps: false,
