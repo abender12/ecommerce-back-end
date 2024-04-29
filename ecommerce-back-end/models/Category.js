70a2d46bb07e184ee7d3a569a03bf73b502ffefc
Alicen Bender <alicene.bender@gmail.com>
2024-04-29 17:15:45 -0400

    Added requirements for category js

--- ecommerce-back-end/models/Category.js
+++ ecommerce-back-end/models/Category.js
@@ -15,6 +15,29 @@ Category.init(
     category_name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+        unique: false
+      }
     }
   },
   {
